1e41674292dafb1e3aa078b7ad66861f29b72594
Mariano <marianoperri@gmail.com>
2021-05-25 16:07:14 -0300

    arreglando problemas de merge

---
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/DatosClase.java
@@ -0,0 +1,34 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+public class DatosClase {
+    private String nombre;
+    private String fechaYHora;
+    private Long idProfesor;
+    private Long cupo;
+	
+    
+    public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getFechaYHora() {
+		return fechaYHora;
+	}
+	public void setFechaYHora(String fechaYHora) {
+		this.fechaYHora = fechaYHora;
+	}
+	public Long getIdProfesor() {
+		return idProfesor;
+	}
+	public void setIdProfesor(Long idProfesor) {
+		this.idProfesor = idProfesor;
+	}
+	public Long getCupo() {
+		return cupo;
+	}
+	public void setCupo(Long cupo) {
+		this.cupo = cupo;
+	}
+}
